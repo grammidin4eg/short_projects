dc90494b12726d8b35a95716d2eb9fa9286ca18a
Yuriy <grammidin4eg@gmail.com>
2020-08-14 22:27:10 +0300

    react_balance app

---
+++ react_balance/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App container">
+      <h1>Планировщик затрат</h1>
+      <h2>Баланс</h2>
+      <h2>Планируемый баланс</h2>
+      <h2>Форма добавления</h2>
+      <h2>Таблица расходов</h2>
+    </div>
+  );
+}
+
+export default App;
